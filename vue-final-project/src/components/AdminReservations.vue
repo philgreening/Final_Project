a010529e5beab675d3fb9e033b0d8f18833429d6
Phil Greening <philgreening@hotmail.com>
2023-02-09 00:28:08 +0000

    Admin Transaction loan logic working

--- vue-final-project/src/components/AdminReservations.vue
+++ vue-final-project/src/components/AdminReservations.vue
@@ -7,6 +7,7 @@
                     <th scope="col">Item Name</th>
                     <th scope="col">User Name</th>
                     <th scope="col">Reservation Date</th>
+                    <th scope="col">Confirm Loan</th>
                 </tr>
             </thead>
             <tbody>
@@ -16,10 +17,35 @@
                         <td v-if="user.user_id === reservation.user_id">{{ user.first_name + ` ` + user.last_name }}</td>
                     </template>
                     <td>{{ formatDate(reservation.res_date) }}</td>
+                    <td>
+                        <button type="button" @click="getResIndex(reservation)" class="btn btn-success"
+                                data-bs-toggle="modal" data-bs-target="#confirmLoanModal">Loan Item</button>
+                    </td>
+
                 </tr>
 
             </tbody>
         </table>
+    </div>
+
+    <div class="modal fade" id="confirmLoanModal" tabindex="-1" aria-labelledby="confirmLoanModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="confirmLoanModalLabel">Confirm Loan</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <h1>Confirm Loan? </h1>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                    <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+                    <button type="button" class="btn btn-primary" @click="loanItem()" data-bs-dismiss="modal">Yes
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 
 </template>
@@ -36,6 +62,7 @@ export default {
     data() {
         return {
             reservations: [],
+            reservedItem: [],
             users: []
         }
     },
@@ -80,7 +107,35 @@ export default {
             } else {
                 return '';
             }
-        }
-    }
+        },
+        getResIndex(reservation) {
+            this.reservedItem = reservation;
+            console.log(this.reservedItem);
+        },
+        async returnItem() {
+            console.log("returned: " + this.returnedItem.item_id);
+
+            const data = {
+
+                transaction_status: 'Returned'
+                // item_name: this.returned.item_name,
+                // user_id: userStore.user.id
+            };
+
+            await axios.patch('http://localhost:4000/update-transaction/' + this.returnedItem.transaction_id, data, {
+                headers: {
+                    Authorization: `Bearer ${userStore.authToken}`
+                }
+            })
+                .then(response => {
+                    console.log("res response: ", response.data);
+                    this.updateItemStatus();
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+    },
+ 
 };
 </script>
--- vue-final-project/src/components/AdminTransactions.vue
+++ vue-final-project/src/components/AdminTransactions.vue
@@ -16,16 +16,41 @@
                 <tr v-for="transaction in transactions" :key="transaction.transaction_id">
                     <td>{{ transaction.item_name }}</td>
                     <template v-for="user in users" :key="user.user_id">
-                    <td v-if="user.user_id === transaction.user_id">{{ user.first_name + ` ` + user.last_name }}</td>
+                        <td v-if="user.user_id === transaction.user_id">{{ user.first_name + ` ` + user.last_name }}
+                        </td>
                     </template>
-                    <td>{{ transaction.transaction_status }}</td>
+                    <td :key="transaction.transaction_status">{{ transaction.transaction_status }}
+                        <template v-if="transaction.transaction_status === 'On Loan'">
+                            <button type="button" @click="getIndex(transaction)" class="btn btn-danger"
+                                data-bs-toggle="modal" data-bs-target="#exampleModal">Return</button>
+                        </template>
+                    </td>
                     <td>{{ formatDate(transaction.loan_date) }}</td>
                     <td>{{ formatDate(transaction.due_date) }}</td>
                     <td>{{ formatDate(transaction.returned_date) }}</td>
                 </tr>
-
             </tbody>
         </table>
+    </div>
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Return item</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <h1>Confirm item returned? </h1>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                    <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+                    <button type="button" class="btn btn-primary" @click="returnItem()" data-bs-dismiss="modal">Yes
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 
 </template>
@@ -42,12 +67,16 @@ export default {
     data() {
         return {
             transactions: [],
-            users: []
+            users: [],
+            items: [],
+            returnedItem: [],
+            // returnedTransaction: []
         }
     },
     mounted() {
         this.getAllTransactions();
         this.getAllUsers();
+        this.getAllItems();
     },
     methods: {
         async getAllTransactions() {
@@ -78,6 +107,21 @@ export default {
                     console.log(error);
                 })
         },
+        async getAllItems() {
+            console.log('token get all: ', userStore.authToken)
+            await axios.get('http://localhost:4000/item/all', {
+                headers: {
+                    Authorization: `Bearer ${userStore.authToken}`
+                }
+            })
+                .then(response => {
+                    console.log(response.data)
+                    this.items = response.data;
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        },
         formatDate(timestamp) {
             if (timestamp) {
                 let date = new Date(timestamp._seconds * 1000)
@@ -86,7 +130,60 @@ export default {
             } else {
                 return '';
             }
-        }
+        },
+
+        getIndex(transaction) {
+            this.returnedItem = transaction;
+            console.log(this.returnedItem);
+        },
+        async returnItem() {
+            console.log("returned: " + this.returnedItem.item_id);
+
+            const data = {
+
+                transaction_status: 'Returned'
+                // item_name: this.returned.item_name,
+                // user_id: userStore.user.id
+            };
+
+            await axios.patch('http://localhost:4000/update-transaction/' + this.returnedItem.transaction_id, data, {
+                headers: {
+                    Authorization: `Bearer ${userStore.authToken}`
+                }
+            })
+                .then(response => {
+                    console.log("res response: ", response.data);
+                    this.updateItemStatus();
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+        async updateItemStatus() {
+            console.log(this.returnedItem.item_id);
+            const data = {
+                status: 'Available'
+            }
+
+            await axios.patch('http://localhost:4000/update-item/' + this.returnedItem.item_id, data, {
+                headers: {
+                    Authorization: `Bearer ${userStore.authToken}`
+                }
+            })
+                .then(response => {
+                    console.log("res response upadte item: ", response.data);
+                    // finds item by id and renders new status to view 
+                    for (let i = 0; i < this.items.length; i++) {
+                        if (this.transactions[i].transaction_id == this.returnedItem.transaction_id) {
+                            this.transactions[i].transaction_status = 'Returned';
+                        }
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+
     }
 };
 </script>
